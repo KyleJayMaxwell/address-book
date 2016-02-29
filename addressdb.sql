f7aae59d2775aab0a9d3e662b85beb4c434b16c9
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-02-29 15:53:37 -0700

    database and tables made

---
+++ addressdb.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS address_book;
+CREATE DATABASE address_book;
+
+\connect address_book;
+
+CREATE TABLE contacts (
+  id serial PRIMARY KEY,
+  first_name varchar(70) NOT NULL,
+  last_name varchar(70) NOT NULL,
+  phone int NOT NULL,
+  email varchar(70) NOT NULL,
+  img_url varchar(70) NOT NULL
+);
+
+CREATE TABLE addresses (
+  id serial PRIMARY KEY,
+  line_1 varchar(70) NOT NULL,
+  line_2 varchar(70) NOT NULL,
+  city varchar(70) NOT NULL,
+  state varchar(70) NOT NULL,
+  zip_code int
+);
+
