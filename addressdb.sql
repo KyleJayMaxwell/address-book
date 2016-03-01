e753d234963442d3227e9d26c174cc721b4fb0cc
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-02-29 19:45:51 -0700

    data seeded with pictures, and index page built

--- addressdb.sql
+++ addressdb.sql
@@ -7,7 +7,7 @@ CREATE TABLE contacts (
   id serial PRIMARY KEY,
   first_name varchar(70) NOT NULL,
   last_name varchar(70) NOT NULL,
-  phone int NOT NULL,
+  phone bigint NOT NULL,
   email varchar(70) NOT NULL,
   img_url varchar(70) NOT NULL
 );
@@ -18,6 +18,7 @@ CREATE TABLE addresses (
   line_2 varchar(70) NOT NULL,
   city varchar(70) NOT NULL,
   state varchar(70) NOT NULL,
+  country varchar(70) NOT NULL,
   zip_code int
 );
 
@@ -25,4 +26,28 @@ CREATE TABLE homes (
   contact_id integer references contacts(id) on delete cascade,
   address_id integer references addresses(id) on delete cascade
 );
+
+INSERT INTO contacts (id, first_name, last_name, phone, email, img_url) VALUES
+(DEFAULT, 'Kyle', 'Maxwell', 7209843866, 'kylejaymaxwell@gmail.com', '../images/kyle.jpg'),
+(DEFAULT, 'Tobin', 'Bode', 1351849327, 'Bode.T@email.com', '../images/tobin.jpg'),
+(DEFAULT, 'Howell', 'Bartoletti', 6821889373, 'howell.bartoletti@email.net', '../images/howell.jpg'),
+(DEFAULT, 'Lue', 'Effertz', 8073602406, 'lue.e@email.io', '../images/lue.jpg'),
+(DEFAULT, 'Alvah', 'Kuhlman', 4070756014, 'kuhlman.alvah@email.com', '../images/alvah.jpg'),
+(DEFAULT, 'Vernon', 'Kertzmann', 188958925, 'vernonvernon@email.com', '../images/vernon.jpg');
+
+INSERT INTO addresses (id, line_1, line_2, city, state, country, zip_code) VALUES
+(DEFAULT, '13724 West Auburn Avenue', '', 'Lakewood', 'CO', 'USA', 80228),
+(DEFAULT, '37632 North Table Court', '#102', 'New York City', 'NY', 'USA', 94356),
+(DEFAULT, '734 South Thomas Street', '#592', 'Phoenix', 'AZ', 'USA', 48536),
+(DEFAULT, '2734 N. Sunset Boulevard', '', 'San Jose', 'CA', 'USA', 02347),
+(DEFAULT, '1 Tower Place', '#9002', 'Paris', '', 'FR', 77777);
+
+INSERT INTO homes (contact_id, address_id) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 5);
+
 
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var options = {};
+
+var pgp = require('pg-promise')(options);
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/address_book';
+
+var db = pgp(connectionString);
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.any('SELECT * FROM homes JOIN contacts ON homes.contact_id = contacts.id JOIN addresses ON addresses.id = homes.address_id ORDER BY contacts.id ASC;')
+  .then(function (data) {
+    res.render('index', { 
+      title: 'Address Book',
+      homes: data
+    });
+  })
+  .catch(function (err){
+    return next(err);
+  });
 });
 
 module.exports = router;
